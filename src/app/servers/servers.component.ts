15c16f06a533431d91504c8c83ec298de2de1565
ArturoTorresMartinez <efrainarturotorres@gmail.com>
2019-06-03 16:55:06 -0500

    Directories

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-servers',
-  template: `<app-server></app-server>
-  <app-server></app-server>`,
-  styleUrls: ['./servers.component.css']
+  selector: "app-servers",
+  templateUrl: "./servers.component.html",
+  styleUrls: ["./servers.component.css"]
 })
 export class ServersComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
+  allowNewServer = false;
+  serverCreationStatus = "No server created";
+  serverName = "TestServer";
+  serverCreated = false;
+  servers = ["Testserver"];
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
   }
 
+  ngOnInit() {}
+  onCreateServer() {
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus =
+      "Server was created, name is " + this.serverName;
+  }
+  onUpdateServerName(event: any) {
+    // console.log(event); Whole Event
+    // console.log(event.data); Event that triggered method
+    // console.log(event.target.value); Full value of the input
+    this.serverName = event.target.value;
+  }
 }
